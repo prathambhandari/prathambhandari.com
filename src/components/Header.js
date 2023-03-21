9ce86a7a7266774ab765dbe0538e50772ba920f3
prathambhandari <prathamkrishnabhandari@gmail.com>
2023-03-21 21:25:55 +0530

    added animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return(
-        <div className="fixed bg-stone-800 text-slate-50 p-5 w-screen flex justify-around items-center">
+        <div className="fixed z-40 bg-opacity-90 bg-slate-900 text-slate-50 p-5 w-screen flex justify-around items-center">
             <h1 className="text-3xl">Pratham</h1>
             <ul className="flex w-80 justify-around">
                 <li className=" cursor-pointer hover:text-slate-300">Home</li>
--- src/components/Home.js
+++ src/components/Home.js
@@ -16,9 +16,14 @@ class Home extends Component{
     }
     render(){
         return(
-            <div className="flex flex-col justify-center items-center bg-slate-100 h-screen">
+            <div>
+            <div className="flex flex-col justify-center items-center bg-slate-100  h-screen p-24">
+            <li className=" h-48 w-48 bg-slate-200 blur-sm rounded-3xl self-start animate-spin-slow list-none"></li>
                     <h1 className="text-7xl font-bold p-10">{this.state.title}</h1>
                     <h2 className="text-3xl font-sans">Im web Developer</h2>
+                    <li className=" h-48 w-48 bg-slate-200 blur-sm rounded-3xl self-end animate-spin-slow list-none"></li>
+            </div>
+      
             </div>
         )
     }
