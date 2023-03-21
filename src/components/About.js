cd4cd65caba2c5b027aba32c80c8ac33ca685156
prathambhandari <prathamkrishnabhandari@gmail.com>
2023-03-21 23:17:52 +0530

    small changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About (){
         <div className="flex  flex-col justify-around items-center bg-slate-100 h-screen ">
         <h1 className="text-4xl font-bold text-slate-900 p-10 z-20">About</h1>
         <div className="bg-slate-200 flex flex-col items-center sm:flex-col md:flex-col lg:flex-row 2xl:flex-row p-10  rounded-sm z-20 border-solid border-stone-500 border-2">
-            <img src="https://pbs.twimg.com/profile_images/1269046768546672640/orvrer_K_400x400.jpg" className="z-20 img-fluid rounded-top object-cover rounded-full w-40 h-40 max-h-40 max-w-40 border-solid border-stone-500 border-4" alt=""></img>
+            <img src="https://pbs.twimg.com/profile_images/1269046768546672640/orvrer_K_400x400.jpg" className="z-20 img-fluid rounded-top object-cover rounded-full w-40 h-40 max-h-40 max-w-40 border-solid border-stone-700 border-2" alt=""></img>
             <div className=" p-10 tracking-wide text-lg ">
                 <p className="text-ellipsis w-96">Born in Mangalore, India. Love to code and design Websites,
                    Welll I like to travel,read books,Singing and Guitar,Tbh in singing and guitar im just jack of two trades and master at none.So what else 🤔 ,Well i like the book "Atomic Habits",🫢 I run a channel on Youtube as well all Social Links on the bottom 👇. 
--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ class Home extends Component{
             <div className="flex flex-col justify-center items-center bg-slate-100  h-screen p-36">
             <li className=" h-52 w-52 bg-slate-200 blur-lg rounded-3xl self-start animate-spin-slow list-none z-0"></li>
             <h1 className="font-bold p-10 z-20 text-5xl sm:text-5xl md:text-7xl lg:text-7xl 2xl:text-7xl">{this.state.title}</h1>
-            <h2 className=" font-sans z-20  text-lg sm:text-lg md:text-xl lg:text-xl 2xl:text-xl">Im web Developer,</h2>
+            <h2 className="flex justify-center font-sans z-20 w-96 text-lg sm:text-lg md:text-xl lg:text-xl 2xl:text-xl">Im web Developer,</h2>
              <li className=" h-52 w-52 bg-slate-200 blur-lg rounded-3xl self-end animate-spin-slow list-none z-0"></li>
             </div>
       
