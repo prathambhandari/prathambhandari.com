befaa06dddb9a9b76c0bf62c6d725f2826d6d02a
prathambhandari <prathamkrishnabhandari@gmail.com>
2023-03-22 00:44:42 +0530

    extra changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function Header(){
     return(
-        <div className="fixed z-40 bg-opacity-90 bg-slate-900 text-slate-50 p-5 w-screen flex justify-around items-center">
-            <h1 className="text-3xl cursor-pointer hover:animate-pulse">Pratham</h1>
-            <ul className="flex w-80 justify-around">
+        <div className="fixed z-40 bg-opacity-90 font-medium bg-slate-100  text-slate-900 p-4 w-screen flex justify-start items-center">
+            <h1 className="text-3xl cursor-pointer hover:animate-pulse">Pratham Bhandari</h1>
+            {/* <ul className="flex w-80 justify-around">
                 <li className=" cursor-pointer hover:text-slate-300">Home</li>
                 <li className="cursor-pointer hover:text-slate-300">About</li>
                 <li className="cursor-pointer hover:text-slate-300">Gallery</li>
                <li className="cursor-pointer hover:text-slate-300">Contact</li>
                 
             
-            </ul>
+            </ul> */}
             
         </div>
     )
