ecac8580be4adf7386b7f47765705cb872ae7a2d
prathambhandari <prathamkrishnabhandari@gmail.com>
2023-03-22 12:29:23 +0530

    projects section phase 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
+import Project from "./components/Project";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Home />
       <About/>
+      <Project />
     </div>
   );
 }
