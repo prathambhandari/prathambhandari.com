d1701e67cef9e199396a0f6d1273baa5b18b2b2c
prathambhandari <prathamkrishnabhandari@gmail.com>
2023-12-27 11:02:27 +0530

    social fix

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,7 +5,7 @@ function Socials(){
         <div className="flex justify-center items-start bg-slate-100 p-10">
             <SocialIcon url="https://twitter.com/iampratham24" network="twitter" target="_blank"  fgColor="#000" bgColor="transparent" style={{ height: 60, width: 60 }}/>
             <SocialIcon url="https://www.linkedin.com/in/prathambhandari/" network="linkedin" target="_blank" fgColor="#000" bgColor="transparent"  style={{ height: 60, width: 60 }}/>
-            <SocialIcon url="https://www.instagram.com/prathamkbhandari/" network="instagram" target="_blank" fgColor="#000" bgColor="transparent" style={{ height: 60, width: 60 }}/>
+            <SocialIcon url="https://www.instagram.com/_prathambhandari/" network="instagram" target="_blank" fgColor="#000" bgColor="transparent" style={{ height: 60, width: 60 }}/>
             <SocialIcon url="https://www.youtube.com/channel/UCeVe4cXVtvN0p4Rcw8sQNBw" network="youtube"  target="_blank" fgColor="#000" bgColor="transparent" style={{ height: 60, width: 60 }}/>
             <SocialIcon url="https://github.com/prathambhandari" network="github" target="_blank"  fgColor="#000" bgColor="transparent" style={{ height: 60, width: 60 }}/>
         </div>
