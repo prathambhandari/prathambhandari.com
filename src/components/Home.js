1a3823890660b49ce71b1766ce7197d622ab3db0
prathambhandari <prathamkrishnabhandari@gmail.com>
2025-03-15 11:01:35 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Courses from "./Courses";
+import PortfolioTimeline from "./Timeline";
 
 class Home extends Component{
     constructor(){
@@ -20,9 +21,9 @@ class Home extends Component{
            
             <div className="flex flex-col justify-center items-center bg-black  h-screen p-8 overflow-hidden">
                 <li className=" h-72 w-72 bg-rose-200 blur-3xl rounded-3xl self-start animate-spin-slow list-none z-0"></li>
-                <h1 className="text-white w-auto font-bold p-10 z-20 text-5xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl">{this.state.title}</h1> 
-                <h2 className=" text-white flex justify-center py-3 font-sans z-20 w-96 text-lg sm:text-lg md:text-xl lg:text-xl 2xl:text-xl">Im Developer,</h2>
-                <h1 className=" text-white flex justify-center py-3 font-bold z-20 w-96 text-2xl  sm:text-lg md:text-xl lg:text-xl 2xl:text-xl ">Courses ⬇️</h1>
+                <h1 className="text-white w-auto font-bold p-10  z-20 text-5xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-6xl">{this.state.title}</h1> 
+                <h2 className=" text-white flex justify-center py-3 pt-10 font-sans z-20 w-96 text-lg sm:text-lg md:text-xl lg:text-2xl 2xl:text-2xl">Im Developer,</h2>
+                <PortfolioTimeline />
                 <li className=" h-72 w-72 bg-teal-200 blur-3xl rounded-3xl self-end animate-spin-slow list-none z-0"></li>
             </div>
   
