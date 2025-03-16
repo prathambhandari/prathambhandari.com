d73cdf396db4fe987dc8fff935a8ec3bc5563f4b
prathambhandari <prathamkrishnabhandari@gmail.com>
2025-03-17 02:01:30 +0530

    theme  updated

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,17 @@ function About() {
         <div className="flex flex-col justify-center items-center bg-black py-6 pt-20 px-3 w-full">
             {/* About Card Section with Slate Background and Border */}
             <div className="bg-black w-full max-w-2xl flex flex-col items-center justify-evenly sm:flex-col md:flex-col lg:flex-row p-6 rounded-lg shadow-lg border-2 border-slate-900">
-                <img 
-                    src="https://avatars.githubusercontent.com/u/62651866?v=4" 
-                    className="rounded-full w-32 h-32 border-4 border-stone-300 mb-6 lg:mb-0"
-                    alt="Profile"
-                />
+            <img 
+                src="https://avatars.githubusercontent.com/u/62651866?v=4" 
+                className="w-48 h-52 border-4 border-slate-500 rounded-2xl object-cover mb-6 lg:mb-0"
+                style={{
+        borderImage: "linear-gradient(135deg, rgba(255, 0, 255, 0.6), rgba(0, 255, 255, 0.6), rgba(0, 0, 255, 0.6))",
+        borderImageSlice: 0,
+        borderRadius:'1rem',
+    }}
+                alt="Profile"
+            />
+
                 <div className="p-4 lg:p-6 text-lg text-white text-center lg:text-left w-full lg:w-[60%]">
                     <p className="text-justify leading-relaxed">
                         Born in Mangalore, India. I love coding and video editing. I enjoy traveling, listening to music, and working with tools like 
